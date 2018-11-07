6a3e0afa2be63cbb6f1a6ef2b943927f0dec582d
Friedrich Striewski <friedrich.striewski@bynder.com>
2018-11-07 14:37:51 +0100

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ class App extends React.Component {
   public render() {
 
     const twit = new Twit({
-      consumer_key: 'ViQVPujGb7SUGjGJvuTHsqQjm',
-      consumer_secret: 'c0PUaTcxQseew5DPOGlqZKNZmwMk1NcEo1qWWxeh8NHTDA27nX',
       access_token: '863335407362158596-PfixfMsxaGCmeJjnlQvBA755Qe6Y7R4',
-      access_token_secret: 'RHk4nXvZCQJKODFQ148fi36KQ5pxrTuA4WO7rZt0ASOvQ'
+      access_token_secret: 'RHk4nXvZCQJKODFQ148fi36KQ5pxrTuA4WO7rZt0ASOvQ',
+      consumer_key: 'ViQVPujGb7SUGjGJvuTHsqQjm',
+      consumer_secret: 'c0PUaTcxQseew5DPOGlqZKNZmwMk1NcEo1qWWxeh8NHTDA27nX'
     })
 
     const sleep = () => {
