1040272cb583ad1ab04daabeba32d8d75611e167
Friedrich Striewski <friedrich.striewski@bynder.com>
2018-11-07 16:05:39 +0100

    Move twit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
 import * as React from 'react';
-import * as Twit from 'twit';
+
 import BottomBar from './components/BottomBar';
 import Header from './components/Header';
 import MainStage from './components/mainstage/MainStage';
 import SideBar from './components/sidebar/SideBar';
 import './App.css';
 
-
 class App extends React.Component {
   public render() {
-
-    const twit = new Twit({
-      access_token: '863335407362158596-PfixfMsxaGCmeJjnlQvBA755Qe6Y7R4',
-      access_token_secret: 'RHk4nXvZCQJKODFQ148fi36KQ5pxrTuA4WO7rZt0ASOvQ',
-      consumer_key: 'ViQVPujGb7SUGjGJvuTHsqQjm',
-      consumer_secret: 'c0PUaTcxQseew5DPOGlqZKNZmwMk1NcEo1qWWxeh8NHTDA27nX'
-    })
-
-    const sleep = () => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve()
-        }, 10000)
-      })
-    }
-
-    const fn = async () => {
-      while (true) {
-        console.log('--ping Twitter:')
-        const tweets = await twit.get('search/tweets', { q: '@realDonaldTrump' })
-        console.log(tweets.data)
-        await sleep()
-      }
-    }
-
     return (
       <div>
         <Header />
@@ -41,7 +15,6 @@ class App extends React.Component {
         <MainStage />
         <BottomBar />
       </div>
-
     );
   }
 }
