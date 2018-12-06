8e39f6aeba8e787ef56c01ccf6b85db098b8a9ed
Friedrich Striewski <friedrich.striewski@bynder.com>
2018-12-06 08:53:19 +0100

    Refinement

--- src/connectors/Stream.tsx
+++ src/connectors/Stream.tsx
@@ -8,29 +8,45 @@ interface IRenderProps {
   tweets: any,
 };
 
+interface ITweet {
+  created_at: string; // date time
+  coordinates: string; // ?
+  text: string; // tweet body
+  retweetCount: number;
+  id: number; // userid
+  name: string; // user name
+  screen_name: string; // profile name
+  activeSince: string; // user created at
+  location: string; // country
+  followersCount: number;
+}
+
+interface IState { tweets: ITweet[] }
+
 interface IProps { children: (props: IRenderProps) => React.ReactNode };
 
+const initialState = {
+  tweets: []
+}
 
-export default class Stream extends React.Component<IProps, {}> {
-  readonly state = { tweets: {} };
+export default class Stream extends React.Component<IProps, IState> {
+  readonly state = initialState;
 
   constructor(props) {
     super(props);
   }
 
-  parseResponse = response => response
+  parseResponse = response => response.body
 
   byKeyword = async (param) => {
-    this.setState({
-      ...this.state, 
-      tweets: await request
-        .get(`${baseUrl}/tweets`)
-        .query({ keyword: param })
-        .then(response => this.parseResponse(response.body))
-    });
 
-    //   const tweets = await request.get(`${baseUrl}/tweets`).query({ keyword: param }).then(response => this.parseResponse(response.body))
-    //   this.setState({ tweets })
+    const messages = await request
+      .get(`${baseUrl}/tweets`)
+      .query({ keyword: param })
+      .then(response => this.parseResponse(response))
+
+
+    this.setState({ tweets: [...messages] })
   };
 
   render() {
